406b874c8ffea9fad81a9a7f3b423f26626b6648
iii <miselrkba@gmail.com>
2020-08-19 09:53:24 +0200

    finished application

--- src/App.js
+++ src/App.js
@@ -34,7 +34,14 @@ class App extends React.Component {
   };
 
   handleEdit = (id) => {
-    console.log(`handle edit ${id}`);
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    const selectedItem = this.state.items.find((item) => item.id === id);
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true,
+    });
   };
 
   handleDelete = (id) => {
